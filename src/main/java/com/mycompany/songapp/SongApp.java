5f9cd7248dd1000cc33c61e396d87cf62bf8d290
Ciano456 <cianoconnor713@gmail.com>
2024-03-13 20:33:03 +0000

    First Commit! Classes setup

---
+++ src/main/java/com/mycompany/songapp/SongApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.songapp;
+
+/**
+ *
+ * @author cianoconnor
+ */
+public class SongApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
